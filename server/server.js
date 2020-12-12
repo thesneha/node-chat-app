4911ac7f14b73309d6bb2061a112b4b84ecfa21d
sneha <sssnehatiwaari0@gmail.com>
2020-12-12 16:33:52 +0530

    added connected and disconnected events

--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
 const path =require('path');
 const express=require('express');
+const socketIO=require('socket.io');
+const http =require('http');
 
 const publicPath=path.join(__dirname,'..','public');
 console.log(publicPath);
 
 const port =process.env.PORT||3000
-
 const app=express();
-app.listen(port,()=>{
-  console.log(`app is running on port ${port}`);
-})
+var server=http.createServer(app);
 
 
+
+//  var server=app.listen(port,()=>{
+//   console.log(`app is running on port ${port}`);
+// })
+var io=socketIO(server);
 app.use(express.static(publicPath));
+
+io.on('connection',  (socket)=>{
+  console.log('new user connected');
+
+  socket.on('disconnect',  ()=>{
+    console.log('user was disconnected');
+  });
+});
+
+server.listen(port,()=>{
+ console.log(`app is running on port ${port}`);
+})
