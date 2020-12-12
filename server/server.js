c5d9c147c069db3b973dc8a96a48b54f9fe87078
sneha <sssnehatiwaari0@gmail.com>
2020-12-13 01:12:03 +0530

    create message utils added

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path =require('path');
 const express=require('express');
 const socketIO=require('socket.io');
 const http =require('http');
+const {generateMessage} =require('./utils/message');
 
 const publicPath=path.join(__dirname,'..','public');
 console.log(publicPath);
@@ -9,8 +10,6 @@ console.log(publicPath);
 const port =process.env.PORT||3000
 const app=express();
 var server=http.createServer(app);
-
-
 
 //  var server=app.listen(port,()=>{
 //   console.log(`app is running on port ${port}`);
@@ -21,27 +20,13 @@ app.use(express.static(publicPath));
 io.on('connection',  (socket)=>{
   console.log('new user connected');
 
+  socket.emit('newMessage', generateMessage('admin','welcome to chat app'));
 
-  socket.emit('newMessage', {
-    from:'admin',
-    text:'welcome to the chat app',
-    createdAt:new Date().getTime()
-  } );
-
-  socket.broadcast.emit('newMessage', {
-    from:'admin',
-    text:'new member joined',
-    createdAt:new Date().getTime()
-  });
-
+  socket.broadcast.emit('newMessage', generateMessage('admin','new member joined'));
 
   socket.on('createMessage',  function(createMessage){
     console.log('create message', createMessage);
-    io.emit('newMessage', {
-      from:createMessage.from,
-      text:createMessage.text,
-      createdAt:new Date().getTime()
-    });
+    io.emit('newMessage', generateMessage(createMessage.from,createMessage.text));
 
     // socket.broadcast.emit('newMessage', {
     //   from:createMessage.from,
