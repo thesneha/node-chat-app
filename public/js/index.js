879a76e0ab78c71a107a17ac34334ae9588a15ec
sneha <sssnehatiwaari0@gmail.com>
2020-12-22 11:37:53 +0530

    scroll top to bottomif user close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket= io();
 
+function scrollToBottom(){
+  var messages =jQuery('#messages');
+  var newMessage =messages.children('li:last-child');
+
+  var clientHeight =messages.prop('clientHeight');
+  var scrollTop =messages.prop('scrollTop');
+  var scrollHeight =messages.prop('scrollHeight');
+  var newMessageHeight=newMessage.innerHeight();
+  var lastMessageHeight=newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight)
+  {
+    messages.scrollTop(scrollHeight);
+  }
+
+};
+
 socket.on('connect',  function(){
   console.log('new user connected');
 
@@ -18,6 +35,7 @@ socket.on('newMessage',  function(newMessage){
     createdAt:formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
 
   // var formattedTime =moment(newMessage.createdAt).format('h:mm a');
   // var li=jQuery('<li></li>');
@@ -34,6 +52,7 @@ socket.on('newLocationMessage',  function(message){
     createdAt:formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
 
 
   // var li=jQuery('<li></li>');
